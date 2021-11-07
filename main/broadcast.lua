f9e6152afb7e64e7218372c2cadfc5ac8be6edf4
dmitry <startking@mail.ru>
2021-11-07 18:04:03 +0300

    Убрал лишнюю проверку

--- main/broadcast.lua
+++ main/broadcast.lua
@@ -1,5 +1,5 @@
 -- Broadcast
--- Использую __index для перехвата обращения к элементам таблицы.
+-- Использую __index для перехвата обращения к элементам которых нет в таблице.
 -- Можно наплодить каналов что будут висеть в памяти. Это минус.
 -- Я думаю стоит сделать создание и удаление канала отдельно. Или метод remove_unused для чистки от пустых каналов.
 
@@ -7,13 +7,9 @@ local channel = require "main.channel"
 
 local M = {}
 
-M.channels = {}
-
 function M:__index(name)
-	if self.channels[name] == nil then
-		self.channels[name] = channel.new()
-	end
-	return self.channels[name]
+	self[name] = channel.new()
+	return self[name]
 end
 
 return setmetatable(M, M) -- Думаю так правильно?
