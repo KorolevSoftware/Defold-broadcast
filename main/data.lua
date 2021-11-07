4fd7d26faba7f62c38a2e616368b5bca266bc52b
dmitry <startking@mail.ru>
2021-11-07 12:22:15 +0300

    Вынес все константы в файл

---
+++ main/data.lua
@@ -0,0 +1,12 @@
+-- Data
+
+local M = {}
+
+M.TINT_RED = vmath.vector4(1, 0, 0, 1)
+M.TINT_GREEN = vmath.vector4(0, 1, 0, 1)
+M.SPHERE_COUNT = 20
+M.GROUP_B = hash("B")
+M.GROUP_A = hash("A")
+M.X_SPAWN_OFFSET = 700
+M.Y_SPAWN_OFFSET = 64
+return M
